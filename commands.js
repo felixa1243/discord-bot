ecd86c00eb37ced1e1f397bfd5327bec88b936a6
felixa1243 <felixarajiph@gmail.com>
2022-09-30 21:50:51 +0700

    "(+)add commands list (-)delete some obselete file"

---
+++ commands.js
@@ -0,0 +1,10 @@
+import {client} from './index.js'
+export const commands = [
+    {
+      name: 'ping',
+      description: 'Test if bot correctly added',
+    },{
+        name:'hot',
+        description:'look for popular list of movie'
+    }
+  ];  
--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 import {
     Client,
-    GatewayIntentBits
-} from 'discord.js'
+    GatewayIntentBits,
+    Routes
+} from 'discord.js';
+import {
+    REST
+} from '@discordjs/rest';
 import {
  config
-}from './config/environment.js'
+}from './config/environment.js';
+import { commands } from './commands.js';
 //client
-const client = new Client({intents:GatewayIntentBits.Guilds})
+export const client = new Client({
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
 //ready state
 client.once('ready',()=>{
     console.log('ready!')
-    
-})
+});
 //login
-client.login(config.token)
+//slash commands
+
+const rest = new REST({ version: '10' }).setToken(config.token);
+
+async function main(){
+    try{
+        console.log('Started refreshing app (/) commands ...');
+        await rest.put(Routes.applicationCommands(config.clientId,config.guildId),{
+            body:commands
+        });
+        console.log('Successfully reloaded application (/) commands.')
+        client.login(config.token)
+
+    } catch(err){
+        console.error(err)
+    }
+}
+main();
+//events handling
+
+client.on('messageCreate',message=>{
+    console.log(`${message.author.username} > ${message.content} - ${message.createdAt.toDateString()}`);
+   });
+client.on('channelCreate',channel=>{
+       console.log(`channel ${channel.name} created at ${channel.createdAt.toDateString()} `);
+   });
+client.on('channelDelete',channel=>{
+       console.log(`channel ${channel.name} deleted`);
+   });
+client.on('guildMemberAdd',member=>{
+       console.log(`user ${user.name} is added to server`);
+   });
+client.on('ready',()=>{
+       console.log(`${client.user.tag} has logged in!`);
+   });
+
+client.on('interactionCreate',interaction=>{
+    if(interaction.isChatInputCommand()){
+        interaction.reply({content:'pong!'})
+    }
+  });
